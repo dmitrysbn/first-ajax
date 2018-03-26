d2eb901f23a72e476c18dc196d9fabd7d44b5bf0
dmitrysbn <dmitry.sbn@gmail.com>
2018-03-26 13:45:49 -0400

    Step 8

--- ajax.js
+++ ajax.js
@@ -3,10 +3,18 @@ document.addEventListener("DOMContentLoaded", function() {
   var step12Button = document.querySelector('#step12button');
   var step3456Button = document.querySelector('#step3456button');
   var step7Button = document.querySelector('#step7button');
+  var step8Button = document.querySelector('#step8button');
+
   var section3456 = document.querySelector('#step3456');
+
   var section7 = document.querySelector('#step7');
   section7.append(document.createElement('p'));
   var section7Paragraph = section7.querySelector('p');
+
+  var section8 = document.querySelector('#step8');
+  section8.append(document.createElement('p'));
+  var section8Paragraph = section8.querySelector('p');
+
 
   step12Button.addEventListener('click', function() {
     $.ajax({
@@ -41,17 +49,29 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 
   step7Button.addEventListener('click', function() {
-      $.ajax({
-        url: 'http://first-ajax-api.herokuapp.com/count',
-        method: 'GET',
-        data: {},
-        dataType: 'text'
-      }).done(function(responseData) {
-        section7Paragraph.innerText = responseData;
-      });
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/count',
+      method: 'GET',
+      data: {},
+      dataType: 'text'
+
+    }).done(function(responseData) {
+      section7Paragraph.innerText = responseData;
+    });
   });
 
+  step8Button.addEventListener('click', function() {
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/time',
+      method: 'GET',
+      data: {timezone: 'America/Toronto'},
+      // data: {},
+      dataType: 'text'
 
+    }).done(function(responseData) {
+      section8Paragraph.innerText = responseData;
+    });
+  });
 
 
 });
