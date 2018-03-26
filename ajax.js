bf37f2a40576357ef69afabeea060740dd87b2d5
dmitrysbn <dmitry.sbn@gmail.com>
2018-03-26 13:05:12 -0400

    Step 3

--- ajax.js
+++ ajax.js
@@ -1,8 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  var runAjaxRequestToRootButton = document.querySelector('#step12button');
+  var step12Button = document.querySelector('#step12button');
+  var step3456Button = document.querySelector('#step3456button');
 
-  runAjaxRequestToRootButton.addEventListener('click', function() {
+  step12Button.addEventListener('click', function() {
     $.ajax({
       url: 'http://first-ajax-api.herokuapp.com/',
       method: 'GET',
@@ -10,6 +11,22 @@ document.addEventListener("DOMContentLoaded", function() {
       dataType: 'text'
     });
   });
+
+  step3456Button.addEventListener('click', function() {
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/ping',
+      method: 'GET',
+      data: {},
+      dataType: 'text'
+    }).done(function(responseData) {
+      console.log(responseData);
+      var p = document.createElement("p");
+      p.innerText = responseData;
+      document.querySelector('#step3456').append(p);
+    })
+
+
+  })
 
 
 
